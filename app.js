9bfc543ff99a0180df36520940df72d8924886ec
wingsd99 <koyo.gsd@gmail.com>
2023-05-02 18:53:49 +0900

    expressフレームワークをappで呼び出せるようにリファクタリング

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // expressフレームワークを利用
 const express = require('express');
-const app = require('express')();
+const app = express();
 const http = require('http').createServer(app);
 // socket.ioライブラリを利用。イベント駆動のリアルタイムな通信を行える
 const io = require('socket.io')(http);
@@ -17,7 +17,7 @@ io.on('connection', (socket) => {
   socket.on('playerSelect', (playerHand) => {
     console.log(playerHand); // 開発中
     // もう一人のクライアントに送信するために変数を宣言
-    const enemyHand = playerHand; 
+    const enemyHand = playerHand;
     console.log(enemyHand); // 開発中
     // 'enemySelect'イベントを発火し、enemyHand(※変数名)をもう一人のクライアントに送信する
     // 「broadcast」をつけることで送信者('playerSelect'イベントを発火した本人)以外を指定できる
