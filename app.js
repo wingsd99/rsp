0d171ec00f320556bb276d9aed98c1045984c64b
wingsd99 <koyo.gsd@gmail.com>
2023-05-09 10:16:39 +0900

    勝敗の判定をサーバ側で行うように変更

--- app.js
+++ app.js
@@ -1,31 +1,142 @@
-// expressフレームワークを利用
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-// socket.ioライブラリを利用。イベント駆動のリアルタイムな通信を行える
-const io = require('socket.io')(http);
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-// 3000番ポートで通信
 const PORT = 3000;
-// ブラウザからのリクエストにviews/index.htmlを返す
+
 app.use(express.static('./views/'));
-// クライアントがアクセスしたことを検知
+
+// できればグローバル変数は使いたくない
+// イベントからuser変数を脱出させる方法を模索中
+// これからroom機能を作るならさらに問題になる
+// userA.roomで部屋を分ける方法もある
+const userA = {
+  id: '',
+  hand: '',
+  result: ''
+}
+const userB = {
+  id: '',
+  hand: '',
+  result: ''
+}
+
 io.on('connection', (socket) => {
-  const id = socket.id;
-  console.log(id);
-  // 'playerSelect'イベントを受信し、playerHand(※変数名)を受け取る
+  console.log(socket.id);
+  const clientsCount = io.engine.clientsCount;
+  console.log(clientsCount);
   socket.on('playerSelect', (playerHand) => {
-    console.log(playerHand); // 開発中
-    // もう一人のクライアントに送信するために変数を宣言
-    const enemyHand = playerHand;
-    console.log(enemyHand); // 開発中
-    // 'enemySelect'イベントを発火し、enemyHand(※変数名)をもう一人のクライアントに送信する
-    // 「broadcast」をつけることで送信者('playerSelect'イベントを発火した本人)以外を指定できる
-    socket.broadcast.emit('enemySelect', enemyHand);
+    const createUserA = () => {
+      userA.id = socket.id;
+      userA.hand = playerHand;
+    };
+    const createUserB = () => {
+      userB.id = socket.id;
+      userB.hand = playerHand;
+    };
+
+    const win = () => {
+      userA.result = 'WIN';
+      userB.result = 'LOSE';
+    };
+    const draw = () => {
+      userA.result = 'DRAW';
+      userB.result = 'DRAW';
+    };
+    const lose = () => {
+      userA.result = 'LOSE';
+      userB.result = 'WIN';
+    };
+
+    const judgeMatch = () => {
+      if (userA.hand === 'Rock') {
+        if (userB.hand === 'Rock') draw();
+        if (userB.hand === 'Scissors') win();
+        if (userB.hand === 'Paper') lose();
+      }
+      if (userA.hand === 'Scissors') {
+        if (userB.hand === 'Rock') lose();
+        if (userB.hand === 'Scissors') draw();
+        if (userB.hand === 'Paper') win();
+      }
+      if (userA.hand === 'Paper') {
+        if (userB.hand === 'Rock') win();
+        if (userB.hand === 'Scissors') lose();
+        if (userB.hand === 'Paper') draw();
+      }
+      io.to(userA.id).emit('matchResult', [userA.result, userB.hand]);
+      io.to(userB.id).emit('matchResult', [userB.result, userA.hand]);
+    };
+
+    const clearUser = () => {
+      userA.id = '';
+      userA.hand = '';
+      userA.result = '';
+
+      userB.id = '';
+      userB.hand = '';
+      userB.result = '';
+    }
+    
+    if (userA.id === '') {
+      createUserA();
+      console.log(userA);
+      console.log('userA is created');
+      io.to(userB.id).emit('enemySelect');
+    } else {
+      createUserB();
+      console.log(userB);
+      console.log('userB is created');
+      judgeMatch();
+      clearUser();
+      console.log('finish');
+    }
+    // class UserInfo {
+    //   constructor(id, hand, result) {
+    //     this.id = id;
+    //     this.hand = hand;
+    //     this.result = result;
+    //   }
+    //   get getUser() {
+    //     return this.id;
+    //   }
+    //   get getHand() {
+    //     return this.hand;
+    //   }
+    //   get getResult() {
+    //     return this.result;
+    //   }
+    //   set setResult(result) {
+    //     this.result = result;
+    //   }
+    // };
+    // const createUserA = () => {
+    //   const userA = new UserInfo(socket.id, playerHand);
+    //   return userA;
+    // };
+    // const createUserB = () => {
+    //   const userB = new UserInfo(socket.id, playerHand);
+    //   return userB;
+    // };
+    // const judgeMatch = () => {
+    //   userA.setResult = 'win';
+    //   userB.setResult = 'lose';
+    //   io.emit('matchResult', result);
+    // };
+    // if (typeof userA === 'undefined') {
+    //   createUserA();
+    //   console.log('userA is created');
+    // } else {
+    //   createUserB();
+    //   console.log('userB is created');
+    //   judgeMatch();
+    //   console.log('finish');
+    // }
   });
 });
 
-// 「localhost:3000」でブラウザからアクセスできるようにする
-http.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`PORT: ${PORT}`);
 });
