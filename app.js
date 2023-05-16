3ee41db4aa183df1d96a683f2778d5a6da74dcde
wingsd99 <koyo.gsd@gmail.com>
2023-05-16 11:50:05 +0900

    top.html追加&req.params.idに関する部分を追加

--- app.js
+++ app.js
@@ -6,35 +6,64 @@ const io = require('socket.io')(server);
 
 const PORT = 3000;
 
-app.use(express.static('./views/'));
+app.use(express.static('./public'));
+// app.use(express.static('./views'));
 
-// できればグローバル変数は使いたくない
-// イベントからuser変数を脱出させる方法を模索中
-// これからroom機能を作るならさらに問題になる
-// userA.roomで部屋を分ける方法もある
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/top.html');
+});
+
+app.get('/index/:id', (req, res) => {
+  const roomNumber = req.params.id;
+  // if (userA.room === '') {
+  //   userA.room = roomNumber;
+  //   socket.join(roomNumber);
+  //   socket.emit('yourRoom', roomNumber);
+  // } else {
+  //   userB.room = roomNumber;
+  //   socket.join(roomNumber);
+  //   socket.emit('yourRoom', roomNumber);
+  // }
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 const userA = {
   id: '',
   hand: '',
-  result: ''
-}
+  result: '',
+  room: ''
+};
 const userB = {
   id: '',
   hand: '',
-  result: ''
-}
+  result: '',
+  room: ''
+};
 
 io.on('connection', (socket) => {
-  console.log(socket.id);
   const clientsCount = io.engine.clientsCount;
-  console.log(clientsCount);
+  console.log(clientsCount, socket.id);
+  // console.log(socket.rooms);
+  // if (req.params.id = 1) {
+  //   socket.join('room1');
+  // } else if (req.params.id = 2) {
+  //   socket.join('room2');
+  // } else if (req.params.id = 3) {
+  //   socket.join('room3');
+  // }
+  
   socket.on('playerSelect', (playerHand) => {
     const createUserA = () => {
       userA.id = socket.id;
       userA.hand = playerHand;
+      socket.join(userA.room);
+      console.log(socket.rooms);
     };
     const createUserB = () => {
       userB.id = socket.id;
       userB.hand = playerHand;
+      socket.join(userB.room);
+      console.log(socket.rooms);
     };
 
     const win = () => {
@@ -74,26 +103,34 @@ io.on('connection', (socket) => {
       userA.id = '';
       userA.hand = '';
       userA.result = '';
-
+      userA.room = '';
+      
       userB.id = '';
       userB.hand = '';
       userB.result = '';
-    }
-    
+      userB.room = '';
+    };
+
     if (userA.id === '') {
       createUserA();
-      console.log(userA);
-      console.log('userA is created');
-      io.to(userB.id).emit('enemySelect');
+      console.log('userA was created');
     } else {
       createUserB();
-      console.log(userB);
-      console.log('userB is created');
+      console.log('userB was created');
       judgeMatch();
+      console.log("userA", userA);
+      console.log("userB", userB);
       clearUser();
       console.log('finish');
     }
-    // class UserInfo {
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`PORT: ${PORT}`);
+});
+
+// class UserInfo {
     //   constructor(id, hand, result) {
     //     this.id = id;
     //     this.hand = hand;
@@ -127,16 +164,10 @@ io.on('connection', (socket) => {
     // };
     // if (typeof userA === 'undefined') {
     //   createUserA();
-    //   console.log('userA is created');
+    //   console.log('userA was created');
     // } else {
     //   createUserB();
-    //   console.log('userB is created');
+    //   console.log('userB was created');
     //   judgeMatch();
     //   console.log('finish');
     // }
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`PORT: ${PORT}`);
-});
